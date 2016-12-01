fa11974f5c26cb87ce2a53b4d9e33c4fff8a80ff
fehrinjt <fehrinjt@co.larimer.co.us>
2016-12-01 11:43:22 -0700

    changed to azure api project

---
+++ API/API/Controllers/TestController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [RoutePrefix("api/test")]
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var testString = "This is a test of the API";
+
+                return Ok(testString);
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var testString = "This is a test of the API which passes in an ID of: " + id;
+
+                return Ok(testString);
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
